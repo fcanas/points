0268febc3b49b6c4965240ae6fae486129065679
Fabian Canas <fcanas@gmail.com>
2015-09-03 14:45:34 -0400

    Initial commit

---
+++ Points/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Points
+//
+//  Created by Fabian Canas on 9/3/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Points/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Points
+//
+//  Created by Fabian Canas on 9/3/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
