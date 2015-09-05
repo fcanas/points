bafb185a834ece2ab8cb4bd01e375b1e515130a3
Fabian Canas <fcanas@gmail.com>
2015-09-05 15:05:20 -0400

    Archive and unarchive document from NSData

--- Points/Document.swift
+++ Points/Document.swift
@@ -36,14 +36,22 @@ class Document: NSDocument {
     override func dataOfType(typeName: String, error outError: NSErrorPointer) -> NSData? {
         // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
         // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-        return nil
+        // outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        
+        return NSKeyedArchiver.archivedDataWithRootObject(nodes as NSArray)
     }
 
     override func readFromData(data: NSData, ofType typeName: String, error outError: NSErrorPointer) -> Bool {
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
+        //
+        
+        if let n = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? [Node] {
+            nodes = n
+            return true
+        }
+        
         outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
         return false
     }
